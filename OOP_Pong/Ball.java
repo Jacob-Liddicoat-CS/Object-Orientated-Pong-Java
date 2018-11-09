3fae7d5c7299bab8ce60bd74c8a1b7163e69b875
Jacob-Liddicoat-CS <j.liddicoat@share.epsb.ca>
2018-11-09 11:50:57 -0700

    Changed Ball Constructor, draw, & gamePlay

--- OOP_Pong/Ball.java
+++ OOP_Pong/Ball.java
@@ -15,31 +15,41 @@ class Ball () {
   private static int ballCount = 5;
 
   //Constructor
-  Ball () {
-    this.ballX = ballX;
-    this.ballY = ballY;
-    this.ballSpeedX = ballSpeedX;
-    this.ballSpeedY = ballSpeedY;
+  Ball (float width, float height) {
+    ballStartX = int(width/2);
+    ballStartY = int(height/2);
+    this.ballX = ballStartX;
+    this.ballY = ballStartY;
+    ballDiameter = int(width/70);
+    this.ballSpeedX = int (random (1, 5));
+    this.ballSpeedY = int (random (1, 5));
+    this.c = color(int(random(255)), int(random(255)), int(random(255)));
   } //End of Constructor
+
+  void draw() {
+    fill(c);
+    ellipse(ballX, BallY, BallDiameter, BallDiameter);
+  }
 
   //Game Start
   void gameStart () {
-    ballStartX = width/2;
-    ballStartY = height/2;
-    ballX = ballStartX;
-    ballY = ballStartY;
-    ballDiameter = width/70;
-    ballSpeedX = 1;
-    ballSpeedY = 1;
-
     ballRandom.Random();
   } //End gameStart
 
   void gamePlay() {
-    ballMoveX = ballSpeedX*directionX;
-    ballMoveY = ballSpeedY*directionY;
-    ballx += ballMoveX;
-    ballY += ballMoveY;
+    //Top and Bottom Boundary Bounce, accounting for increased ball movement per "step"
+    if (ballY <= 0 || ballY >= height) { //Empty IF to skip ball arithmetic when score happens
+    } else {
+      if (ballY <= 0+(ballDiameter/2) || ballY >= height-(ballDiameter/2) ) {
+        directionY = directionY * (-1);
+      }
+      if (ballY <= 0+(ballDiameter/2) ) {
+        ballY = 0+(ballDiameter/2);
+      }
+      if (ballY >= height-(ballDiameter/2) ) {
+        ballY = height-(ballDiameter/2);
+      }
+    }
   }
 
   //Getters and Setters
